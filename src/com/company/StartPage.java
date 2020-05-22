aee94aff7adbeb3a72f6dee158df7fddeaa397b2
Daria Shevchenko <dashaawork@gmail.com>
2020-05-23 00:01:46 +0300

    improving StartPage

--- src/com/company/StartPage.java
+++ src/com/company/StartPage.java
@@ -5,64 +5,91 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class StartPage  extends JPanel {
 
-    JFrame window;
-    Container con;
-    JPanel titleNamePanel, startButtonPanel;
-    JLabel titleNameLabel;
+/**
+ * Created by Shevchenko Daria on 22.05.2020.
+ */
+public class StartPage extends JFrame {
+
+    JPanel panel1;
     Font titleFont = new Font("Times New Roman", Font.PLAIN,90);
     Font normalFont = new Font("Times New Roman", Font.PLAIN,30);
-    JButton startButton;
 
 
-    public StartPage(){
 
-        window = new JFrame();
-        window.setSize(800,600);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.getContentPane().setBackground(Color.BLACK);
-        window.setLayout(null);
-        window.setVisible(true);
-        con = window.getContentPane();
+    // конструктор класу для апп, створення основного вікна
+    StartPage(){
+        super("RGB");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800,500);
 
-        titleNamePanel = new JPanel();
-        titleNamePanel.setBounds(100,100,600,150);
-        titleNamePanel.setBackground(Color.BLACK);
-        titleNameLabel = new JLabel("MAZE");
-        titleNameLabel.setForeground(Color.WHITE);
-        titleNameLabel.setFont(titleFont);
-
-        startButtonPanel = new JPanel();
-        startButtonPanel.setBounds(300,400,200,100);
-        startButtonPanel.setBackground(Color.BLACK);
-
-        startButton = new JButton("START");
-        startButton.setBackground(Color.black);
-        startButton.setForeground(Color.white);
-        startButton.setFont(normalFont);
-
-        startButton.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-
-            }
-        });
-
-
-        titleNamePanel.add(titleNameLabel);
-        startButtonPanel.add(startButton);
-
-        con.add(titleNamePanel);
-        con.add(startButtonPanel);
+        start();
 
     }
 
-    public void createNewGamePage(){
+    public void start(){
+        if (panel1!=null) {
+            remove(panel1);
+        }
+        setPreferredSize(new Dimension(800,600));
 
+        startPage();
+        add(panel1);
+        this.pack();
+        setVisible(true);
+    }
+
+    /**
+     * запускає стартову сторінку
+     */
+    private void startPage(){
+
+        panel1 = new JPanel();
+        panel1.setBackground(Color.black);
+
+        JLabel title = new JLabel("RGB");
+        title.setBounds(300,100,600,150);
+        title.setForeground(Color.WHITE);
+        title.setFont(titleFont);
+
+        JButton startButton =new JButton("START!");
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.WHITE);
+        startButton.setFont(normalFont);
+
+        startButton.setBounds(300,400,200,80);
+        startButton.addActionListener(e -> firstMaze());
+
+        panel1.add(title);
+        panel1.add(startButton);
+        panel1.setLayout(null);
+
+    }
+
+    /**
+     * відкриває 1 лабіринт
+     */
+    private void firstMaze(){
+        panel1.removeAll();
+        revalidate();
+        repaint();
+
+
+        JButton back =new JButton("Back");
+        back.setBounds(650,500,95,30);
+        back.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+
+                start();
+
+            }
+        });
+        panel1.add(back);
+        panel1.setLayout(null);
+        revalidate();
+        repaint();
     }
 
 
 
 }
-
-
